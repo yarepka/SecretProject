ff65edafd230fc383031e1bf565e7c9d1a37034f
Роман Бубнов <yarepka01@gmail.com>
2020-04-12 13:30:45 +0300

    Updated app.js, added get methods for home, register and login pages

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
 
 app.listen(3000, () => {
     console.log("Server started on post 3000.");
