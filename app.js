712216ce53e2c9d9ca75397cf6e3a2f1af5ec523
Роман Бубнов <yarepka01@gmail.com>
2020-04-12 14:02:26 +0300

    Level 1 - Register Users with Username and Password(really bad practice, because user data stored at the database as a plain text, so everybody, who have acces to the database can get the data about users)

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const userSchema = mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -18,6 +27,37 @@ app.get("/login", (req, res) => {
 
 app.get("/register", (req, res) => {
     res.render("register");
+});
+
+app.post("/register", (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save((err) => {
+        if(err) console.log(err);
+        else res.render("secrets")
+    });
+});
+
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    
+    User.findOne({
+        email: username,
+    }, (err, foundUser) => {
+        if(!err) {
+            if(foundUser) {
+                if(foundUser.password === password) {
+                    res.render("secrets");
+                }
+            }
+        } else {
+            console.log(err);
+        }
+    });
 });
 
 app.listen(3000, () => {
