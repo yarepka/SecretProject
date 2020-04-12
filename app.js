1ed160723b4a92198e1e81bfaa6c80a67367e1a9
Роман Бубнов <yarepka01@gmail.com>
2020-04-12 13:20:30 +0300

    Initialization of Authentication Secrets project

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.listen(3000, () => {
+    console.log("Server started on post 3000.");
+});
